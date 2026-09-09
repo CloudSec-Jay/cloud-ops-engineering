4bcf9170cc6889a3720c740bb1e6e9b4577c52fd
Jason <201442056+CloudSec-Jay@users.noreply.github.com>
2026-09-08 20:06:57 -0500

    Add IAM policy for Terraform state KMS key
    Added IAM policy document for KMS key permissions.

--- infrastructure/terraform/aws/bootstrap/state.tf
+++ infrastructure/terraform/aws/bootstrap/state.tf
@@ -35,4 +35,34 @@ resource "aws_kms_key" "terraform_state" {
   description             = "KMS key for Terraform state encryption"
   deletion_window_in_days = 10
   enable_key_rotation     = true
+  policy                  = data.aws_iam_policy_document.terraform_state_kms.json
 }
+
+data "aws_iam_policy_document" "terraform_state_kms" {
+  statement {
+    sid    = "Enable IAM User Permissions"
+    effect = "Allow"
+    principals {
+      type        = "AWS"
+      identifiers = ["arn:aws:iam::${data.aws_caller_identity.current.account_id}:root"]
+    }
+    actions   = ["kms:*"]
+    resources = ["*"]
+  }
+
+  statement {
+    sid    = "Allow Terraform State Encryption"
+    effect = "Allow"
+    principals {
+      type        = "Service"
+      identifiers = ["s3.amazonaws.com"]
+    }
+    actions = [
+      "kms:Decrypt",
+      "kms:GenerateDataKey"
+    ]
+    resources = ["*"]
+  }
+}
+
+data "aws_caller_identity" "current" {}
